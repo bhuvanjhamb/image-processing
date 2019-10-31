7b9cf85bcac6ff286d1c6b157b8abda45566cb06
bhuvanjhamb <56886429+bhuvanjhamb@users.noreply.github.com>
2019-10-31 21:42:53 +0530

    Submission for shape_detect.py

--- shape_detect.py
+++ shape_detect.py
@@ -1,25 +1,72 @@
-#!/usr/bin/env python
-import numpy as np
+#bhuvan's submission for eyantra hactober fest (image processing)
+
 import cv2
+import numpy as np
+def circleDetect():# detecting shape  on the basis of number of sides 
+    if 10 < len(approx) < 20:
+        cv2.putText(frame, "Circle", (x, y), font, 1, (0, 0, 0))
+def squareDetect():
+    if len(approx) == 4:
+        cv2.putText(frame, "square", (x, y), font, 1, (0, 0, 0))
+
+def nothing(x):
+    # any operation
+    pass
 
 cap = cv2.VideoCapture(0)
+#trackbars have been made to adjust lower and higher HSV limit as per lightening conditions for my case the best results or obtained are [32,26,0] and [130 255 255] being the lower and higher HSV range respectievely 
+cv2.namedWindow("Trackbars")
+cv2.createTrackbar("L-H", "Trackbars", 0, 180, nothing)
+cv2.createTrackbar("L-S", "Trackbars", 66, 255, nothing)
+cv2.createTrackbar("L-V", "Trackbars", 134, 255, nothing)
+cv2.createTrackbar("U-H", "Trackbars", 180, 180, nothing)
+cv2.createTrackbar("U-S", "Trackbars", 255, 255, nothing)
+cv2.createTrackbar("U-V", "Trackbars", 243, 255, nothing)
 
-def circleDetect():
-
-
-def squareDetect():
-
+font = cv2.FONT_HERSHEY_COMPLEX
 
 while True:
-	ret, frame = cap.read()
+    _, frame = cap.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-	circleDetect()
+    l_h = cv2.getTrackbarPos("L-H", "Trackbars")
+    l_s = cv2.getTrackbarPos("L-S", "Trackbars")
+    l_v = cv2.getTrackbarPos("L-V", "Trackbars")
+    u_h = cv2.getTrackbarPos("U-H", "Trackbars")
+    u_s = cv2.getTrackbarPos("U-S", "Trackbars")
+    u_v = cv2.getTrackbarPos("U-V", "Trackbars")
 
-	squareDetect()
+    lower_red = np.array([l_h, l_s, l_v])
+    upper_red = np.array([u_h, u_s, u_v])
 
-	cv2.imshow('frame', frame)
+    mask = cv2.inRange(hsv, lower_red, upper_red)#creating the mask
+    kernel = np.ones((5, 5), np.uint8)
+    mask = cv2.erode(mask, kernel)
 
-	cv2.waitKey(3)
+    # Contours detection
+    if int(cv2.__version__[0]) > 3:
+        # Opencv 4.x.x
+        contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)#finding contours
+    else:
+        # Opencv 3.x.x
+        _, contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        approx = cv2.approxPolyDP(cnt, 0.02*cv2.arcLength(cnt, True), True)
+        x = approx.ravel()[0]
+        y = approx.ravel()[1]
+
+        if area > 400:
+            cv2.drawContours(frame, [approx], 0, (0, 0, 0), 5)
+        circleDetect()
+        squareDetect()
+    cv2.imshow("Frame", frame)
+    cv2.imshow("Mask", mask)
+
+    key = cv2.waitKey(3)
+    if key == 27:
+        break
 
 cap.release()
 cv2.destroyAllWindows()
